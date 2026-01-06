92bb63c9765569df7a4fb442bd39b5c13a2dd242
Emilis <emiliszaks@gmail.com>
2026-01-06 17:44:35 +0200

    Player(bugs)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,69 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Transform PlayerPrefab;
+    [Header("Movement")]
+    public float walkSpeed = 5f;
+    public float runSpeed = 9f;
+
+    [Header("Jumping")]
+    public float jumpForce = 5f;
+
+    [Header("Crouching")]
+    public float crouchHeight = 1f;
+    private float standingHeight = 1.6f;
+
+    [Header("Camera")]
+    public Camera playerCamera;
+
+    [Header("Checks")]
+    private Rigidbody rb;
+    private bool isCrouching = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
 
     private void Update()
     {
-        transform.position = PlayerPrefab.transform.position + new Vector3(0, 1, -5);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouching = !isCrouching;
+        }
+
+        if(isCrouching)
+        {
+            playerCamera.transform.localPosition = new Vector3(0, crouchHeight, 0);
+        }
+        else
+        {
+            playerCamera.transform.localPosition = new Vector3(0, standingHeight, 0);
+        }
     }
+
+    private void FixedUpdate()
+    {
+        float speed;
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = runSpeed;
+        }
+        else
+        {
+            speed = walkSpeed;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+    }
+
 }
