3913b2511902aaa1c7f90b9449ee609a3f255ca5
Inkaras <emiliszaks@gmail.com>
2026-01-11 21:50:15 +0200

    CraftingStarters

--- Assets/Scripts/CraftPickaxe.cs
+++ Assets/Scripts/CraftPickaxe.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class CraftPickaxe : MonoBehaviour
@@ -9,16 +10,22 @@ public class CraftPickaxe : MonoBehaviour
     public Item crafted;
     public void CraftItem()
     {
-        if (InventoryManager.Instance.HasItem(itemChk, 100) && InventoryManager.Instance.HasItem(itemChk2, 100))
+        if (InventoryManager.Instance.HasItem(itemChk, 100) && InventoryManager.Instance.HasItem(itemChk2, 200))
         {
             InventoryManager.Instance.RemoveItem(itemChk, 100);
-            InventoryManager.Instance.RemoveItem(itemChk2, 100);
-            InventoryManager.Instance.AddItem(crafted, 1);
-            //Debug.Log("Crafted: " + crafted.itemName);
+            InventoryManager.Instance.RemoveItem(itemChk2, 200);
+            StartCoroutine(CraftDelay());
         }
         else
         {
             Debug.Log("Not enough materials to craft " + crafted.itemName);
         }
+    }
+
+    IEnumerator CraftDelay()
+    {
+        yield return new WaitForSeconds(2f);
+        InventoryManager.Instance.AddItem(crafted, 1);
+        Debug.Log("Crafted: " + crafted.itemName);
     }
 }
